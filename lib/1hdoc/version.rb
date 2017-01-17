42545d47e3cfec1efcaf3d0ace417604dcdbf787
Dom <dom.corvasce@yandex.com>
2017-01-17 17:32:25 +0100

    Rename latest version (another time

--- lib/1hdoc/version.rb
+++ lib/1hdoc/version.rb
@@ -1,7 +1,7 @@
 ##
 # Provides all necessary features implemented by 1hdoc.
 module HDOC
-  VERSION = '0.2.1b'.freeze
+  VERSION = '0.2.2'.freeze
   CODENAME = 'Phoenix'.freeze
 
   def self.version
