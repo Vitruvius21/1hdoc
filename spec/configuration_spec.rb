b4d131facfd3bcd2ad7b37210faa33768be27f79
Dom <dom.corvasce@yandex.com>
2017-01-17 18:33:18 +0100

    Clear Configuration and relative spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -6,17 +6,17 @@ describe HDOC::Configuration do
   after  { File.delete @target_file if File.exist? @target_file }
 
   it 'should raise an error if the given file is not found' do
-    expect { described_class.new('~/.123x.yml') }.to raise_error(Errno::ENOENT)
-  end
-
-  it 'should accept a custom file parser' do
-    described_class.init(@target_file, JSON, it_works: true)
-    configuration = described_class.new(@target_file, JSON)
-
-    expect(configuration.options['it_works']).to eq(true)
+    expect { described_class.new('~/.123x.yml') }.to raise_error(/Unable to find/)
   end
 
   context '.init' do
+    it 'should accept a custom file parser' do
+      described_class.init(@target_file, JSON, it_works: true)
+      configuration = described_class.new(@target_file, JSON)
+
+      expect(configuration.options['it_works']).to eq(true)
+    end
+
     it 'should initialize a new configuration file' do
       described_class.init(@target_file)
       expect(File.exist?(@target_file)).to eq(true)
@@ -24,7 +24,6 @@ describe HDOC::Configuration do
 
     it 'should write the given options on the configuration file' do
       described_class.init(@target_file, it_works: true)
-      expect(File.read(@target_file)).to include('it_works')
     end
   end
 
