dfd10398d4967af9d98be99768857b8654e9ec58
Dom <dom.corvasce@yandex.com>
2017-02-05 12:25:42 +0100

    Fix #8: Show a warn if user already pushed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,6 @@
 ### Changed
 - Fix a typo which raised an error if no configuration file was found.
 
+# 0.2.4, 5th Feb 2017
+### Changed
+- Now 1hdoc warn user if she tries to push multiple times.
--- lib/1hdoc/actions/push.rb
+++ lib/1hdoc/actions/push.rb
@@ -3,8 +3,20 @@ module HDOC
     ##
     # Push the progress to the repository.
     def push
+      if user_already_pushed?
+        $stderr.puts 'You already pushed for today'
+        return
+      end
+
       open_repository
       @repository.push
+    end
+
+    private
+
+    def user_already_pushed?
+      config = Configuration.new(ENVIRONMENT[:configuration_file])
+      config.options[:last_commit_on] == Time.now.strftime('%Y-%m-%d')
     end
   end
 end
